2f16b7f214e14255d7c33dcae315a88339890592
Edgar Benitez <edgarbenitez92@gmail.com>
2024-09-02 22:03:12 -0400

    Intl-tel-input
    -. New component Int-tel-input has been created to implement the new custom component

---
+++ src/app/components/intl-tel-input/intl-tel-input.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-intl-tel-input',
+  standalone: true,
+  imports: [],
+  templateUrl: './intl-tel-input.component.html',
+  styleUrl: './intl-tel-input.component.scss'
+})
+export class IntlTelInputComponent {
+
+}
